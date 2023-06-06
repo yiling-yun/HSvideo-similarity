07db24d92ad69457dc9d5e5966c5a5c79cd188a9
LUN42H <lun42hou@gmail.com>
2023-06-05 19:14:03 -0700

    change labeling

--- ratingLabeling/js/stim_main.js
+++ ratingLabeling/js/stim_main.js
@@ -46,9 +46,9 @@ const LABEL_LIST = [
     "Bother", "Scratch", "Fight", "Flirt with", "Examine", "Tickle", "Throw", "Herd", "Avoid"
 ];
 const BUTTON_LIST = [
-    "#b1", "#b2", "#b3", "#b4", "#b5", "#b6", "#b7", "#b8", "#b9",
+    "#b0", "#b1", "#b2", "#b3", "#b4", "#b5", "#b6", "#b7", "#b8", "#b9",
     "#b10", "#b11", "#b12", "#b13", "#b14", "#b15", "#b16", "#b17", "#b18",
-    "#b19", "#b20", "#b21", "#b22", "#b23", "#b24", "#b25", "#b26", "#b27"
+    "#b19", "#b20", "#b21", "#b22", "#b23", "#b24", "#b25", "#b26"
 ]
 const TRIAL_INPUT    = VID_LIST.sort((a, b)   => 0.5 - Math.random());
 const SHUFFLED_LABEL = LABEL_LIST.sort((a, b) => 0.5 - Math.random());
